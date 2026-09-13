07c26b2c1f18905ded1faf8743451db72bbacbcf
Natalya Kim <kim87324@yandex.ru>
2026-09-14 01:39:36 +0300

    feat: add pagination

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import {initData} from "./data.js";
 import {processFormData} from "./lib/utils.js";
 
 import {initTable} from "./components/table.js";
+import { initPagination } from "./components/pagination.js";
 // @todo: подключение
 
 
@@ -19,9 +20,14 @@ const {data, ...indexes} = initData(sourceData);
  */
 function collectState() {
     const state = processFormData(new FormData(sampleTable.container));
+    const rowsPerPage = parseInt(state.rowsPerPage);
+    const page = parseInt(state.page ?? 1);
+
 
     return {
-        ...state
+        ...state,
+        rowsPerPage,
+        page
     };
 }
 
@@ -33,6 +39,7 @@ function render(action) {
     let state = collectState(); // состояние полей из таблицы
     let result = [...data]; // копируем для последующего изменения
     // @todo: использование
+    result = applyPagination(result, state, action);
 
 
     sampleTable.render(result)
@@ -46,7 +53,17 @@ const sampleTable = initTable({
 }, render);
 
 // @todo: инициализация
-
+const applyPagination = initPagination(
+    sampleTable.pagination.elements,
+    (el, page, isCurrent) => {
+        const input = el.querySelector('input');
+        const label = el.querySelector('span');
+        input.value = page;
+        input.checked = isCurrent;
+        label.textContent = page;
+        return el;
+    }
+);
 
 const appRoot = document.querySelector('#app');
 appRoot.appendChild(sampleTable.container);
