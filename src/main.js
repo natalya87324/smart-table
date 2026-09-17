56edca28e30586aaf94912d528631d94bab14e81
Natalya Kim <kim87324@yandex.ru>
2026-09-17 15:42:39 +0300

    feat: add searching

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import {initTable} from "./components/table.js";
 import { initPagination } from "./components/pagination.js";
 import { initSorting } from "./components/sorting.js";
 import { initFiltering } from "./components/filtering.js";
+import { initSearching } from "./components/searching.js";
 // @todo: подключение
 
 
@@ -41,6 +42,7 @@ function render(action) {
     let state = collectState(); // состояние полей из таблицы
     let result = [...data]; // копируем для последующего изменения
     // @todo: использование
+    result = applySearching(result, state, action);
     result = applyFiltering(result, state, action);
     result = applySorting(result, state, action);
     result = applyPagination(result, state, action);
@@ -78,6 +80,7 @@ const applySorting = initSorting([
     sampleTable.header.elements.sortByTotal
 ]);
 
+const applySearching = initSearching('search');
 
 
 const appRoot = document.querySelector('#app');
