8294fdb56bfbd9d22cc5bdb7e94bb3729aa368dc
Natalya Kim <kim87324@yandex.ru>
2026-09-17 14:35:37 +0300

    feat: add filtering

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import {processFormData} from "./lib/utils.js";
 import {initTable} from "./components/table.js";
 import { initPagination } from "./components/pagination.js";
 import { initSorting } from "./components/sorting.js";
+import { initFiltering } from "./components/filtering.js";
 // @todo: подключение
 
 
@@ -40,6 +41,7 @@ function render(action) {
     let state = collectState(); // состояние полей из таблицы
     let result = [...data]; // копируем для последующего изменения
     // @todo: использование
+    result = applyFiltering(result, state, action);
     result = applySorting(result, state, action);
     result = applyPagination(result, state, action);
 
@@ -67,10 +69,16 @@ const applyPagination = initPagination(
     }
 );
 
+const applyFiltering = initFiltering(sampleTable.filter.elements, {
+    searchBySeller: indexes.sellers
+});
+
 const applySorting = initSorting([
     sampleTable.header.elements.sortByDate,
     sampleTable.header.elements.sortByTotal
 ]);
+
+
 
 const appRoot = document.querySelector('#app');
 appRoot.appendChild(sampleTable.container);
